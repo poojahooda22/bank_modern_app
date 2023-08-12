1298f663555a6a7adcc11faa91c6f4891e92a1a7
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-08-12 22:34:02 +0530

    Revert "for deploying including dist"
    This reverts commit 003ae4e778ccddad38678710feda6fdb07373bd7.

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
-  base: '/bank_modern_app/',
   plugins: [react()],
 })
